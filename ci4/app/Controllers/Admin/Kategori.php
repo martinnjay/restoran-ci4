c05d7d6767988543bd0a4a339e51d9e81e0603cb
Martin Riadi <martinrizk1103@gmail.com>
2020-11-23 09:08:45 +0700

    1.0.0

---
+++ ci4/app/Controllers/Admin/Kategori.php
@@ -0,0 +1,44 @@
+<?php namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Kategori extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	public function select()
+	{
+		# code...
+	}
+
+	public function selectWhere($id = null)
+	{
+		# code...
+	}
+
+	public function formInsert()
+	{
+		# code...
+	}
+
+	public function formUpdate()
+	{
+		# code...
+	}
+
+	public function update($id = null)
+	{
+		# code...
+	}
+
+	public function delete($id = null)
+	{
+		# code...
+	}
+
+	//--------------------------------------------------------------------
+
+}
---
+++ ci4/app/Controllers/Admin/Menu.php
@@ -0,0 +1,22 @@
+<?php namespace App\Controllers;
+
+class Menu extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	public function select()
+	{
+		# code...
+	}
+
+	public function update($id = null, $nama = null)
+	{
+		# code...
+	}
+
+	//--------------------------------------------------------------------
+
+}
